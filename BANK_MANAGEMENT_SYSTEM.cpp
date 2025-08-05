f04d01db14b962a2d9f7360306a4e10d4f2b1e99
pulkitsinha004 <133797832+sinha004@users.noreply.github.com>
2025-08-06 00:42:32 +0530

    BANK_MANAGEMENT_SYSTEM.cpp

--- BANK_MANAGEMENT_SYSTEM.cpp
+++ BANK_MANAGEMENT_SYSTEM.cpp
@@ -404,7 +404,7 @@ void intro()
 	cout<<"\n| BANK";
 	cout<<"\n| MANAGEMENT";
 	cout<<"\n| SYSTEM";
-	cout<<"\n\nMADE BY : Priyanshu Sharma, Raghvan Pareek, Rahil Godha, Swamy Pulaparthi";
+	cout<<"\n\nMADE BY : Pulkit Sinha, Priyanshu Sharma, Raghvan Pareek, Swamy Pulaparthi";
 	cout<<"\n\nPress ENTER to continue...";
     cin.get();
 }
